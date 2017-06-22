d5cea2387eea051c730e9943e96417924ed95809
msjo91 <msjo91@gmail.com>
2017-06-22 13:59:44 +0900

    Add document strings for function description

--- QTR1/exercise/0621_func_practice.py
+++ QTR1/exercise/0621_func_practice.py
@@ -1,4 +1,5 @@
 def f1(li):
+    """Return the number of odd elements in a given list."""
     num = 0
     for i in li:
         if i % 2 == 1:
@@ -7,12 +8,14 @@ def f1(li):
 
 
 def f2(li):
+    """Print each odd element in a given list."""
     for i in li:
         if i % 2 == 1:
             print(i)
 
 
 def f3(li):
+    """Return the sum of all odd elements in a given list."""
     num = 0
     for i in li:
         if i % 2 == 1:
@@ -21,6 +24,7 @@ def f3(li):
 
 
 def f4(li):
+    """Return the sum of all index positions whose corresponding element is odd in a given list."""
     idx = 0
     for i in li:
         if i % 2 == 1:
@@ -29,13 +33,15 @@ def f4(li):
 
 
 def f5(li):
-    sq = []
+    """Return the same list where each element has been squared."""
+    sq_li = []
     for i in li:
-        sq.append(i ** 2)
-    return sq
+        sq_li.append(i ** 2)
+    return sq_li
 
 
 def f6(li):
+    """Return the largest number in a given list."""
     mx = li[0]
     for i in li:
         if i > mx:
@@ -44,6 +50,7 @@ def f6(li):
 
 
 def f7(li):
+    """Return the average of all the numbers in a given list."""
     sm = 0
     num = len(li)
     for i in li:
@@ -52,12 +59,14 @@ def f7(li):
 
 
 def f8(a, b, n):
+    """Print all the numbers divisible by n within the range a and b inclusive."""
     for i in range(a, b):
         if i % n == 0:
             print(i)
 
 
 def f9(width, height):
+    """Print an ASCII rectangle with the given width and height."""
     if width == 0:
         for i in range(0):
             print('*' * width)
@@ -67,11 +76,13 @@ def f9(width, height):
 
 
 def f10(n):
+    """Print a triangle with the given height n."""
     for i in range(n):
         print('*' * (i + 1))
 
 
 def f11(li):
+    """Return True if a list is sorted in descending order and False otherwise. Return True for an empty list."""
     if len(li) == 0 or len(li) == 1:
         return True
     for i in range(len(li) - 1):
@@ -82,6 +93,7 @@ def f11(li):
 
 
 def f12(li):
+    """Return True if a list consists of all negative nubers and False otherwise. Return True for an empty list."""
     if len(li) == 0:
         return True
     for i in li:
@@ -92,6 +104,7 @@ def f12(li):
 
 
 def f13(li, target):
+    """Return the index of the last occurrence of target in a list."""
     if li.count(target) == 0:
         print("Misfire!")
     else:
@@ -102,8 +115,9 @@ def f13(li, target):
 
 
 def f14(li):
+    """Return the index of the last negative number in a list."""
     if all(i >= 0 for i in li):
-        print("No negative numbers!")
+        print("Give negative numbers!")
     else:
         for i in range(len(li)):
             if li[i] < 0:
@@ -112,6 +126,7 @@ def f14(li):
 
 
 def f15(li):
+    """Return the sum of all the elements at even index positions."""
     even_sum = 0
     for i in range(len(li)):
         if i % 2 == 0 and i != 0:
@@ -120,17 +135,20 @@ def f15(li):
 
 
 def f16(n):
+    """Print out an upside down triangle."""
     for i in reversed(range(n)):
         print('*' * (i + 1))
 
 
 def f17(li):
+    """Print out every unique element in a list in reverse order."""
     for i in reversed(li):
         if li.count(i) == 1:
             print(i)
 
 
 def f18(n):
+    """Return n!."""
     if n < 0:
         print("Seriously? Go google 'gamma function'!")
     else:
@@ -141,6 +159,7 @@ def f18(n):
 
 
 def f19(li):
+    """Print the factorial of each element of a given list."""
     if any(i < 0 for i in li):
         print("Again? Google 'factorial negative'!")
     else:
@@ -153,6 +172,7 @@ def f19(li):
 
 
 def f20(li):
+    """Print a countdown starting from each element to zero for a given list."""
     for i in li:
         for n in reversed(range(i + 1)):
             if n == 0:
@@ -162,6 +182,7 @@ def f20(li):
 
 
 def f21(li1, li2):
+    """Return a new list where each index in the new list corresponds to list1[index] + list2[index]."""
     if len(li1) != len(li2):
         print("Both lists should have the same length!")
     else:
@@ -172,12 +193,14 @@ def f21(li1, li2):
 
 
 def f22(n):
+    """Print all the numbers from 1 to n inclusive that is a multiple of 2 or 3."""
     for i in range(2, n + 1):
         if i % 2 == 0 or i % 3 == 0:
             print(i)
 
 
 def f23(li):
+    """Return the largest value in the list (of all the nested lists inside list)."""
     if any(type(sublist) != list for sublist in li):
         print("All elements are list type, period!")
     else:
@@ -193,6 +216,7 @@ def f23(li):
 
 
 def f24(li):
+    """Return the second largest value in the list."""
     if len(li) < 2:
         print("The list should have at least 2 elements!")
     else:
@@ -209,6 +233,7 @@ def f24(li):
 
 
 def f25(n):
+    """Return the leftmost digit in n."""
     try:
         return int(str(n)[0])
     except ValueError:
@@ -216,6 +241,7 @@ def f25(n):
 
 
 def f26(li):
+    """Print the largest value of each of the nested lists in a given list."""
     if any(type(sublist) != list for sublist in li):
         print("All elements should be list type!")
     elif any(sublist == [] for sublist in li):
