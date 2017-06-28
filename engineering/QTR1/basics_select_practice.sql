aabc6ea98b598e4b3c9010f081b922edb8db23ef
msjo91 <msjo91@gmail.com>
2017-06-28 15:43:27 +0900

    Add notes and finish discussion 3

--- engineering/QTR1/basics_select_practice.sql
+++ engineering/QTR1/basics_select_practice.sql
@@ -1,27 +1,35 @@
+/* Followed after basics.sql */
+
+/* select, distinct(no repetition), order by */
 select address from student;
 select distinct address from student;
 select address from student order by address;
 select distinct address from student order by address;
 
+/* select fields (= attributes, columns)*/
 select name, 2012-year_emp from professor order by name;
 select name, 2012-year_emp years from professor order by years;
 
+/* cartesian product and aliasing*/
 select * from student s, department d where s.dept_id = d.dept_id;
 
+/* Find the student id of junior students in comp.sci dept */
 select stu_id from student s, department d where s.dept_id = d.dept_id and dept_name = '컴퓨터공학과' and year = 3;
 
+/* desc ordering and ordering within ordering */
 select name, stu_id from student order by name desc, stu_id;
 
+/* Find name and address of students with the same name */
 select s1.name, s1.address, s2.name, s2.address from student s1, student s2 where s1.name = s2.name and s1.stu_id > s2.stu_id;
 
+/* Find a speicific student's classes which the student received an A+ */ 
 select title from student s, takes t, class cl, course co where s.stu_id = t.stu_id and t.class_id = cl.class_id and cl.course_id = co.course_id and  s.name='김광식' and grade='A+';
 
+/* LIKE operator */
 select * from student where name like '김%';
-
 select * from student where resident_id like '%-2%';
 
+/* null search */
 select * from takes where grade is null;
-
 select * from takes where grade is not null;
-
 select stu_id from takes where grade is null;
--- engineering/QTR1/discussion3.sql
+++ engineering/QTR1/discussion3.sql
@@ -1,6 +1,7 @@
 drop table if exists sales cascade;
 drop table if exists store cascade;
 
+/* 3-1. Create tables store and sales */
 create table store
 (
     storeNum varchar(20),
@@ -25,8 +26,21 @@ create table sales
     constraint pk_sales primary key (timeKey, itemNum, storeNum, employeeNum)
 );
 
+/* 3-2. Add and drop columns */
 alter table store add contact varchar(20);
 alter table sales drop column tax_amount;
 
+/* 3-3. Drop the tables */
 drop table sales;
 drop table store;
+
+/* Followed after basics.sql */
+
+/* 3-4. Insert records into professor */
+insert into professor values ('92003', '851221-2812312', '유강민', '920', '조교수', 2017);
+insert into professor values ('92303', '600842-1293821', '최지헌', '923', '교수', 2000);
+insert into professor values ('92503', '590605-2934212', '이한빛', '925', '교수', 1996);
+
+/* 3-5. Give 1 more credit for 3-credit-courses */
+update course set credit = credit + 1 where credit = 3;
+/* Note : Once updated, it cannot be undone! (For now.) */
