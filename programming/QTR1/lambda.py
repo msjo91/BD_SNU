1fcef77b15a9d4501e25c750c3f27ef0378e6edf
msjo91 <msjo91@gmail.com>
2017-07-12 11:12:56 +0900

    Lambda basics

---
+++ programming/QTR1/lambda.py
@@ -0,0 +1,12 @@
+"""
+Lambda function
+A nameless, inline, single-use function
+lambda parameter1, parameter2, ...: definition
+"""
+# Case 1
+f1 = lambda x, y: x * y
+
+
+# Above is same as (except for the name):
+def f2(x, y):
+    return x * y
