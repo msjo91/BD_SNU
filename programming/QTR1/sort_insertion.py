d3219af8a7cb6ffc3510ef6359f2b17a79708322
msjo91 <msjo91@gmail.com>
2017-07-14 16:36:39 +0900

    Insertion, quick sorts with counters

--- programming/QTR1/sort_insertion.py
+++ programming/QTR1/sort_insertion.py
@@ -5,13 +5,24 @@ Step 2 : Move each element from the unsorted list one-by-one.
 Step 3 : Keep the sorted list sorted.
 
 Note : The unsorted list is preserved while having a new sorted list.
-Note : The below code will just swap places.
+Note : The below code will just swap places (It works this way as well).
 """
 
 
 def insertion_sort(lst):
+    s_counter = 0
+    c_counter = 0
     for i in range(1, len(lst)):
         j = i
+        if lst[j] >= lst[j - 1]:
+            c_counter += 1
         while j > 0 and lst[j] < lst[j - 1]:
             lst[j], lst[j - 1] = lst[j - 1], lst[j]
             j -= 1
+            s_counter += 1
+            c_counter += 1
+    return lst, "Total: {t} (Swaps: {s}, Compares: {c})".format(
+        t=s_counter + c_counter,
+        s=s_counter,
+        c=c_counter
+    )
--- programming/QTR1/sort_quick.py
+++ programming/QTR1/sort_quick.py
@@ -6,8 +6,16 @@ Step 3 : Repeat step 2 in each new lists.
 Step 4 : Merge.
 """
 
+c_counter = 0
+a_counter = 0
+u_counter = 0
+
 
 def quick_sort(items):
+    global c_counter
+    global a_counter
+    global u_counter
+
     if len(items) > 1:
         pivot_idx = len(items) // 2
         smaller_items = []
@@ -15,11 +23,23 @@ def quick_sort(items):
 
         for i, val in enumerate(items):
             if i != pivot_idx:
+                c_counter += 1
                 if val < items[pivot_idx]:
                     smaller_items.append(val)
+                    a_counter += 1
                 else:
                     larger_items.append(val)
+                    a_counter += 1
+            else:
+                c_counter += 1
 
         quick_sort(smaller_items)
         quick_sort(larger_items)
         items[:] = smaller_items + [items[pivot_idx]] + larger_items
+        u_counter += 1
+        return items, "Total: {t} (Compares: {c}, Appends: {a}, Unions: {u}".format(
+            t=c_counter + a_counter + u_counter,
+            c=c_counter,
+            a=a_counter,
+            u=u_counter
+        )
