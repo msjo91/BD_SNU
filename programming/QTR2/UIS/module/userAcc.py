48faa94afcb9b69db35e94f8f147482ea53f95d2
msjo91 <msjo91@gmail.com>
2017-08-18 17:21:20 +0900

    Objectify user account

---
+++ programming/QTR2/UIS/module/userAcc.py
@@ -0,0 +1,22 @@
+class UserAcc:
+    id_ = 0
+    name = ""
+    role = 0
+    conn = None
+
+    def __init__(self, id_=0, name="", role=0, conn=None):
+        self.id_ = id_
+        self.name = name
+        self.role = role
+        self.conn = conn
+
+    def set_attrs(self, id_, name, role, conn):
+        """Set user attributes."""
+        self.id_ = id_
+        self.name = name
+        self.role = role
+        self.conn = conn
+
+
+# user account object declaration
+user_acc = UserAcc()
