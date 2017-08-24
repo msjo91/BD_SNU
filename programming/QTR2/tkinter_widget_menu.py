cea969e7d12169fd0df1bf965c6fb3435dc4fd75
msjo91 <msjo91@gmail.com>
2017-08-24 10:25:03 +0900

    Add edit and help menu

--- programming/QTR2/tkinter_widget_menu.py
+++ programming/QTR2/tkinter_widget_menu.py
@@ -10,6 +10,7 @@ def donothing():
 root = Tk()
 menubar = Menu(root)
 
+# Create file menu
 filemenu = Menu(menubar, tearoff=0)
 
 filemenu.add_command(label="New", command=donothing)
@@ -26,6 +27,27 @@ filemenu.add_command(label="Exit", command=root.quit)
 
 # Pack menu under label "File" (Drop-down style).
 menubar.add_cascade(label="File", menu=filemenu)
+
+# Create edit menu
+editmenu = Menu(menubar, tearoff=0)
+
+editmenu.add_command(label="Undo", command=donothing)
+editmenu.add_separator()
+editmenu.add_command(label="Cut", command=donothing)
+editmenu.add_command(label="Copy", command=donothing)
+editmenu.add_command(label="Paste", command=donothing)
+editmenu.add_command(label="Delete", command=donothing)
+editmenu.add_command(label="Select All", command=donothing)
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+# Create help menu
+helpmenu = Menu(menubar, tearoff=0)
+
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About", command=donothing)
+
+menubar.add_cascade(label="Help", menu=helpmenu)
 
 # If root.config(menu=menubar) is not declared, system menu does not change.
 root.config(menu=menubar)
