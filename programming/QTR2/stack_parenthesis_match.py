55916f3daf509da22b4210238138fb3f3738f9b4
msjo91 <msjo91@gmail.com>
2017-08-28 14:36:28 +0900

    Use stack to find matching parentheses

---
+++ programming/QTR2/stack_parenthesis_match.py
@@ -0,0 +1,32 @@
+class Stack:
+    """Stack class using list"""
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        if self.items == []:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+
+mystack = Stack()
+
+
+def parenthesis_matching(arg):
+    for i in range(len(arg)):
+        if arg[i] == "(":
+            mystack.push(i)
+        elif arg[i] == ")":
+            if not mystack.is_empty():
+                print((mystack.pop(), i))
