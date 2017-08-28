2e60ab1caefdc421d2ad54c852946f929ed16e81
msjo91 <msjo91@gmail.com>
2017-08-28 15:24:32 +0900

    Create Stack class and TOH class (has setup function)

---
+++ programming/QTR2/stack_towers_of_hanoi.py
@@ -0,0 +1,50 @@
+"""
+Towers of Hanoi.
+
+Rules:
+ Move all disks on one tower to another empty tower.
+ A larger disk cannot stack on a smaller disk.
+ The number of steps should be the least.
+
+ A disk with a smaller number in its name is the smaller disk.
+"""
+
+
+class Stack:
+    """Stack class using list"""
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        if self.items is False:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+
+class TOH:
+    def setup(self, num_disk=3, num_tower=3, start_tower=1):
+        self.num_disk = num_disk
+        self.num_tower = num_tower
+        self.start_tower = start_tower
+
+        tower_dict = {}
+        for i in range(1, self.num_disk + 1):
+            tower_dict["Tower {}".format(i)] = Stack()
+
+        disk_list = []
+        for i in range(1, self.num_tower + 1):
+            disk_list.append("Disk {}".format(i))
+
+        for i in disk_list:
+            tower_dict["Tower {}".format(self.start_tower)].push(i)
