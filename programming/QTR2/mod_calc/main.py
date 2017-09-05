101cf974b50f5ad5ef45bd75748e3490b25f7737
msjo91 <msjo91@gmail.com>
2017-09-06 04:41:54 +0900

    Run GUI

---
+++ programming/QTR2/mod_calc/main.py
@@ -0,0 +1,8 @@
+from tkinter import Tk
+
+from app import App
+
+root = Tk()  # Root window
+root.title('Python Library Calculator')  # Root title
+app = App(root)  # Call App
+root.mainloop()  # Run
