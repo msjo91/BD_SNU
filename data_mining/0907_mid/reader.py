867db819fefd88ca28d5b647e42879bd22e14b18
msjo91 <msjo91@gmail.com>
2017-09-18 10:16:11 +0900

    Remove unnecessary file

--- data_mining/0907_mid/reader.py
+++
@@ -1,6 +0,0 @@
-from csv import reader
-
-with open('maryland_accident_dataset_by_report_no.csv') as f:
-    spamreader = reader(f, delimiter=',', quotechar='|')
-    for row in spamreader:
-        print(', '.join(row))
