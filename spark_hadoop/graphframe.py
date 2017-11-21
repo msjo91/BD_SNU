f491fb696ec1e36e15387097185f5a38b78be7e1
msjo91 <msjo91@gmail.com>
2017-11-21 13:57:45 +0900

    Play with some concepts (rename, aggregation, pagerank, etc.)

--- spark_hadoop/graphframe.py
+++ spark_hadoop/graphframe.py
@@ -5,12 +5,8 @@ ${SPARK_HOME}/bin/pyspark --packages graphframes:graphframes:0.5.0-spark2.1-s_2.
 """
 
 from pyspark.sql import *
+from pyspark.sql import functions as F
 from graphframes import *
-
-# Get airports dataset
-airportsFilePath = "downloads/lab6/airports-codes-na.csv"
-airports = spark.read.csv(airportsFilePath, header='true')
-airports.createOrReplaceTempView("airportsNA")
 
 # Get departure delays dataset
 delaysFilePath = "downloads/lab6/departuredelays.csv"
@@ -23,8 +19,17 @@ query = "SELECT DISTINCT iata FROM (%s) a" % subquery
 tripIATA = spark.sql(query)
 tripIATA.createOrReplaceTempView("tripIATA")
 
+# Get airports dataset
+airportsFilePath = "downloads/lab6/airports-codes-na.csv"
+airports = spark.read.csv(airportsFilePath, header='true')
+airports.createOrReplaceTempView("airportsNA")
+
+# Leave only airports which has delayed departures
+query = "SELECT a.* FROM airportsNA a JOIN tripIATA t ON a.IATA = t.IATA"
+delayports = spark.sql(query)
+
 # A new airports view with renamed columns
-tripVertices = airports.withColumnRenamed("ID", "No").distinct()  # ID -> No
+tripVertices = delayports.withColumnRenamed("ID", "No").distinct()  # ID -> No
 tripVertices = tripVertices.withColumnRenamed("IATA", "id").distinct()  # IATA -> id
 
 # A new delays view with selected columns
@@ -35,3 +40,34 @@ tripEdges = tripEdges.withColumn("label", tripEdges["delay"].cast("int"))
 # Create a graphframe
 tripGraph = GraphFrame(tripVertices, tripEdges)
 
+# The longest delay in dataset
+maxDelay = tripGraph.edges.groupBy().max("delay")
+li = maxDelay.select('max(delay)').collect()
+val = li[0]['max(delay)']
+query = "SELECT * FROM departureDelays WHERE delay = %d" % val
+longestDelay = spark.sql(query)
+longestDelay.show()
+
+# The number of delayed versus on-time/eary flights
+delay_count = tripGraph.edges.filter("delay > 0").count()
+norm_count = tripGraph.edges.filter("delay <= 0").count()
+print("Delayed flights: %d \tOn-time/early flights: %d" % (delay_count, norm_count))
+
+# What flights departing Seattle(SEA) are most likely to have significant delays?
+delaySEA = tripGraph.edges.filter("src = 'SEA' and delay > 0") \
+    .groupBy("src", "dst").avg("delay").orderBy("avg(delay)", ascending=False)
+delaySEA.show()
+
+# Top 5 busiest airports (most flights in and out)
+ins = tripGraph.inDegrees
+ins.createOrReplaceTempView("Ins")
+outs = tripGraph.outDegrees
+outs.createOrReplaceTempView("Outs")
+query = "SELECT i.id, inDegree, outDegree FROM Ins i JOIN Outs o ON i.id = o.id"
+ins_n_outs = spark.sql(query)
+total = ins_n_outs.groupBy('id').agg(F.sum(ins_n_outs.inDegree + ins_n_outs.outDegree))
+total.orderBy('sum((inDegree + outDegree))', ascending=False).limit(5).show()
+
+# Airport rankings by PageRank
+ranks = tripGraph.pageRank(resetProbability=0.15, maxIter=5)
+ranks.vertices.orderBy(ranks.vertices.pagerank.desc()).show()
